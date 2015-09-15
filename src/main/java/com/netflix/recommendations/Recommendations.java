1551a2e3eb6063daf49cb01c74c99b79aee20d4a
Jon Schneider <jkschneider@gmail.com>
2015-09-15 08:50:44 -0400

    Starting point

--- src/main/java/com/netflix/recommendations/Recommendations.java
+++ src/main/java/com/netflix/recommendations/Recommendations.java
@@ -2,78 +2,47 @@ package com.netflix.recommendations;
 
 import java.util.Set;
 
-import javax.inject.Inject;
-
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EurekaStatusChangedEvent;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.common.collect.Sets;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableEurekaClient
-@EnableFeignClients
 public class Recommendations {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Recommendations.class).web(true).run(args);
     }
 
-    @EventListener
-    public void onEurekaStatusDown(EurekaStatusChangedEvent event) {
-        if(event.getStatus() == InstanceInfo.InstanceStatus.DOWN || event.getStatus() == InstanceInfo.InstanceStatus.OUT_OF_SERVICE) {
-            System.out.println("Stop listening to queues and such...");
-        }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
-}
-
-@FeignClient("membership")
-interface MembershipRepository {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/member/{user}")
-    Member findMember(@PathVariable("user") String user);
 }
 
 @RestController
 @RequestMapping("/api/recommendations")
 class RecommendationsController {
-    @Inject
-    MembershipRepository membershipRepository;
+    @Autowired
+    RestTemplate restTemplate;
 
     Set<Movie> kidRecommendations = Sets.newHashSet(new Movie("lion king"), new Movie("frozen"));
     Set<Movie> adultRecommendations = Sets.newHashSet(new Movie("shawshank redemption"), new Movie("spring"));
-    Set<Movie> familyRecommendations = Sets.newHashSet(new Movie("hook"), new Movie("the sandlot"));
 
     @RequestMapping("/{user}")
-    @HystrixCommand(fallbackMethod = "recommendationFallback",
-            ignoreExceptions = UserNotFoundException.class,
-            commandProperties={@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
     public Set<Movie> findRecommendationsForUser(@PathVariable String user) throws UserNotFoundException {
-        Member member = membershipRepository.findMember(user);
+        Member member = restTemplate.getForObject("http://localhost:8000/api/member/{user}", Member.class, user);
         if(member == null)
             throw new UserNotFoundException();
         return member.age < 17 ? kidRecommendations : adultRecommendations;
-    }
-
-    /**
-     * Should be safe for all audiences
-     */
-    Set<Movie> recommendationFallback(String user) {
-        return familyRecommendations;
     }
 }
 
